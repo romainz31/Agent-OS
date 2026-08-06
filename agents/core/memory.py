3817becde91c79aff2573da9feadae436fdba581
Romain ZAMBON <romain_zambon@hotmail.fr>
2026-08-06 19:17:24 +0200

    Mise à jour des notes du module 2.7

---
+++ agents/core/memory.py
@@ -0,0 +1,14 @@
+import json
+
+
+MEMORY_FILE = "data/memory.json"
+
+
+def load_memory():
+    with open(MEMORY_FILE, "r") as file:
+        return json.load(file)
+
+
+def save_memory(memory):
+    with open(MEMORY_FILE, "w") as file:
+        json.dump(memory, file, indent=4)
